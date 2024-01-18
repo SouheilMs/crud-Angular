7c3816d6cb57fd6b49083ed1d4c5871fa9124f58
unknown <souheil000@gmail.com>
2024-01-18 18:47:45 +0100

    consume getTodos in App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Todo } from './model/todos';
+import { ServiceService } from './services/service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  data: Todo[] = []
+  constructor(private service: ServiceService){}
+  ngOnInit(): void {
+    
+    this.service.getAllTodos().subscribe((res: any)=>{
+      this.data = res.todos
+    })
+  }
+}
